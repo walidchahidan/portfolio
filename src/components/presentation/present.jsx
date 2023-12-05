e2308786b4c8c3d4fa72c57187b44a65646d4458
WALID CHAHIDANE <chahidanwalid37@gmail.com>
2023-12-05 15:41:39 +0100

    up

--- src/components/presentation/present.jsx
+++ src/components/presentation/present.jsx
@@ -4,17 +4,16 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 export class Presentation extends Component {
-  componentDidMount(){
-    Aos.init({duration:2000});
-}
+  componentDidMount() {
+    Aos.init({ duration: 2000 });
+  }
   render() {
     return (
       <section id="presentation">
         <div className="div_pres ">
-          
           <h1 className="pres_h">PRESENTATION</h1>
           <hr />
-          <p data-aos="fade-up">
+          {/* <p data-aos="fade-up">
             Je suis <b style={{ color: "gray" }}> Walid Chahidane </b>j'ai 22
             ans,je suis passioné par la programmation et le
             <b style={{ color: "gray" }}> Developpement Web</b> ,actuellement
@@ -25,9 +24,17 @@ export class Presentation extends Component {
             en ligne. Je developpe aussi en <b style={{ color: "gray" }}>PHP/LARAVEL</b>, ce qui me permet de développer
             des sites web complets.
           </p>
-          <p data-aos="fade-up">J'ai quelques projets intéressants à voir. <a href="#projects">ici</a></p>
+          <p data-aos="fade-up">J'ai quelques projets intéressants à voir. <a href="#projects">ici</a></p> */}
+          <p data-aos="fade-up">
+            En tant que développeur web full stack, je maîtrise de nombreux
+            compétencesallant de la création d'interfaces captivantes en HTML,
+            CSS et JavaScript, jusqu'à l'utilisation agile de ReactJS et
+            Next.js, pour des expériences utilisateur réactives et fluides. Mon
+            expertise en Back-End s'étend à Node JS et Express JS aussi que PHP
+            et Laravel. J'ai également une bonne profonde connaissance de
+            MongoDB et My SQL pour la gestion des données.
+          </p>
         </div>
-
       </section>
     );
   }
